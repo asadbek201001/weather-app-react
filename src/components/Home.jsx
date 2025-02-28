8faf0cfbc074e4e29783700a1ee49d5fc2477c5f
AsadbekSWE <asadbekgulomov2010@gmail.com>
2025-02-28 18:30:54 +0500

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,35 +11,28 @@ const Home = () => {
 
     const fetchWeatherApi = async (place) => {
         try {
-          
-       
             const API = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${API_KEY}`;
-            
             const response = await fetch(API);
             if (!response.ok) throw new Error(`API xatolik: ${response.status}`);
 
             const data = await response.json();
             setWeatherData({
                 place,
-                temperature: data.main.temp,
+                temperature: Number((data.main.temp - 273.15).toFixed(1)), 
                 humidity: data.main.humidity,
                 windSpeed: data.wind.speed,
             });
         } catch (error) {
-            console.error("Xatolik yuz berdi -->", error);
+            console.error("Xatolik yuz berdi:", error);
             alert("Ma’lumotlarni olishda muammo bor!");
         }
-    };
-
-    const handleDelete = () => {
-        setWeatherData(null);
     };
 
     return (
         <div className="home">
             <Menu />
             <BookContainer onSearch={fetchWeatherApi} />
-            <NotesContainer weatherData={weatherData} onDelete={handleDelete} />
+            {weatherData && <NotesContainer weatherData={weatherData} onDelete={() => setWeatherData(null)} />}
         </div>
     );
 };
