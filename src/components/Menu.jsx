392180041af17c94d5e5f880bfe647cf4fee20ae
Asadbek.G <asadbekgulomov2010@gmail.com>
2025-05-27 16:41:02 +0500

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,9 +7,6 @@ const Menu = () => {
         <menu>
             <h1 className="name">Weather</h1>
             <h2>
-                <a href="https://github.com/asadbek201001" className="about">About Us</a>
-            </h2>
-            <h2>
                 <a href="https://onlinecompass.app/" style={{ padding: "20px" }} className="chalgish">Compass</a>
             </h2>
         </menu>
